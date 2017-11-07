53cdf2dc7f29ca77496574b465ccb4836e228600
Chritten22 <christian.frostholm.larsen@gmail.com>
2017-11-07 21:13:31 +0100

    Slick + PHP/CSS

--- statue.php
+++ statue.php
@@ -1,56 +1,36 @@
 <!DOCTYPE html>
 <html id="statue-bg">
   <head>
-    <script>
-      $(function() {
-        $('.kc-wrap').KillerCarousel({
-          // Width of carousel.
-          width: 800,
-          // Item spacing in 3D (modern browsers) modes.
-          spacing3d: 120,
-          // Item spacing in 2D (old browsers) modes.
-          spacing2d: 120,
-          showShadow:true,
-          showReflection: true
-        });
-      });
-    </script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>Statuer i farver</title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="stylesheet" href="/css/flexboxgrid.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-    <link href="killercarousel.css" type="text/css" rel="stylesheet" />
-    <script type="text/javascript" src="killercarousel.js"></script>
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
   </head>
   <body>
-    <!-- Main wrapping parent. -->
-    <div class = "kc-wrap">
-    <!-- Carousel items follow. -->
-      <div class="kc-item">
-        <img src="img/statue/statue-1.jpg" alt = "" >
-      </div>
-      <div class="kc-item">
-        <img src="img/statue/statue-2.jpg" alt = "" >
-      </div>
-      <div class="kc-item">
-        <img src="img/statue/statue-3.jpg" alt = "" >
+    <div class="slide-scale">
+      <div class="image-slide">
+        <div><img src="img/statue/statue-1.jpg" class="slide-img"></div>
+        <div><img src="img/statue/statue-2.jpg" class="slide-img"></div>
+        <div><img src="img/statue/statue-3.jpg" class="slide-img"></div>
       </div>
     </div>
-    $(function() {
-      $('.kc-wrap').KillerCarousel({
-        // Width of carousel.
-        width: 800,
-        // Item spacing in 3D (modern browsers) modes.
-        spacing3d: 120,
-        // Item spacing in 2D (old browsers) modes.
-        spacing2d: 120,
-        showShadow:true,
-        showReflection: true
+
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="slick\slick.min.js"></script>
+
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('.image-slide').slick({
+          infinite: true,
+
       });
     });
+  </script>
+
   </body>
 </html>
